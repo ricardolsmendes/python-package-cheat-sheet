77a0c51994181077484b519c97414c707fdf0d91
Ricardo Mendes <ricardolsmendes@gmail.com>
2022-06-05 21:12:39 -0300

    chore: specify the dependency versions

--- setup.py
+++ setup.py
@@ -13,13 +13,17 @@ setuptools.setup(
     },
     include_package_data=True,
     install_requires=(
-        'stringcase',
+        'stringcase ~=1.2.0',
     ),
     setup_requires=(
-        'pytest-runner',
+        'pytest-runner ~=5.3.2',
     ),
     tests_require=(
-        'pytest-cov',
+        'coverage ==6.2',
+        'pytest ~=7.0.1',
+        'pytest-cov ~=2.12.1',
+        'typing-extensions ==4.1.1',
+        'tomli ~=1.2.3',
     ),
     classifiers=[
         'Development Status :: 3 - Alpha',
